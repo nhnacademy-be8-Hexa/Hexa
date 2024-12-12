3b52e7a7a195e6b0038e82f659fee001f75d3acf
조승주 <tmdwn3318@gmail.com>
2024-12-12 12:24:42 +0900

    feat: AuthInfoUtils 생성
    static 메소드로 현재 로그인된 정보를 쉽게 조회 할 수 있게 해주는 유틸 클래스 만듬
    isLogin()
    getUsername()
    getRole()
    많관부

--- src/main/java/com/nhnacademy/hello/controller/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/controller/index/IndexController.java
@@ -1,23 +1,23 @@
 package com.nhnacademy.hello.controller.index;
 
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import com.nhnacademy.hello.common.util.AuthInfoUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+
     @GetMapping(value = {"/index.html","/"})
     public String index(
             Model model
     ){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
         // 로그인 하지 않을 경우 현재 인증 정보 객체는 익명 인증 정보 (AnonymousAuthenticationToken) 이다
-        if(!(authentication instanceof AnonymousAuthenticationToken)){
-            model.addAttribute("userId", authentication.getName());
-            model.addAttribute("role", authentication.getAuthorities().stream().findFirst().get().getAuthority());
+        if(AuthInfoUtils.isLogin()) {
+            // AuthInfoUtils 사용 예시
+            model.addAttribute("userId", AuthInfoUtils.getUsername());
+            model.addAttribute("role", AuthInfoUtils.getRole());
         }
 
         return "index/index";
