496f3b43e77f8546cc6a0157efb1793a704d281b
조승주 <tmdwn3318@gmail.com>
2024-12-23 17:53:00 +0900

    feat: mypage

--- src/main/java/com/nhnacademy/hello/controller/index/IndexController.java
+++ src/main/java/com/nhnacademy/hello/controller/index/IndexController.java
@@ -13,9 +13,7 @@ public class IndexController {
             Model model
     ){
 
-        // 로그인 하지 않을 경우 현재 인증 정보 객체는 익명 인증 정보 (AnonymousAuthenticationToken) 이다
         if(AuthInfoUtils.isLogin()) {
-            // AuthInfoUtils 사용 예시
             model.addAttribute("userId", AuthInfoUtils.getUsername());
             model.addAttribute("role", AuthInfoUtils.getRole());
         }
