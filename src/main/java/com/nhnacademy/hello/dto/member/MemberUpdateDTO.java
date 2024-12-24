aef404911e754c8637c1d7bff97f73695a3d2c24
조승주 <tmdwn3318@gmail.com>
2024-12-24 17:36:08 +0900

    fix: dto valid에 error 메세지 추가

--- src/main/java/com/nhnacademy/hello/dto/member/MemberUpdateDTO.java
+++ src/main/java/com/nhnacademy/hello/dto/member/MemberUpdateDTO.java
@@ -1,20 +1,17 @@
 package com.nhnacademy.hello.dto.member;
 
-import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDate;
 
 public record MemberUpdateDTO(
-        @Length(min = 8, max = 20)
+        @Length(min = 8, max = 20, message = "패스워드는 8~20자이어야 합니다.")
         String memberPassword,
-        @Length(min = 2, max = 20)
-        @NotNull
+        @Length(min = 2, max = 20, message = "이름은 2~20자이어야 합니다.")
         String memberName,
-        @Length(min = 10, max = 11)
-        @NotNull
+        @Length(min = 10, max = 11, message = "전화번호는 10~11자이어야 합니다.")
         String memberNumber,
-        @Length(max = 320)
+        @Length(max = 320, message = "이메일은 320자 이하이어야 합니다.")
         String memberEmail,
         LocalDate memberBirthAt,
         String ratingId,
