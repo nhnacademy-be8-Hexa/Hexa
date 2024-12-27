392fa9b4b626b1d4342760796ad17beca10be818
highsiverr <ye90202@gmail.com>
2024-12-27 10:37:12 +0900

    feat: coupon 관리자 페이지 생성

---
+++ src/main/java/com/nhnacademy/hello/controller/admin/CouponController.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.hello.controller.admin;
+
+import com.nhnacademy.hello.common.feignclient.MemberAdapter;
+import com.nhnacademy.hello.common.util.AuthInfoUtils;
+import com.nhnacademy.hello.dto.member.MemberDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class CouponController {
+    private final MemberAdapter memberAdapter;
+
+    @GetMapping("/admin/coupon")
+    public String adminPage(Model model){
+
+        // 로그인 검사
+        if(!AuthInfoUtils.isLogin()){
+            return "redirect:/login";
+        }
+
+        // 현재 로그인된 사용자 정보 조회
+        MemberDTO memberDTO = memberAdapter.getMember(AuthInfoUtils.getUsername());
+
+        // 관리자인지 검사
+        if(!"ADMIN".equals(memberDTO.memberRole())){
+            return "redirect:/index"; //
+        }
+
+        model.addAttribute("member",memberDTO);
+
+        return "admin/coupon";
+    }
+}
+
