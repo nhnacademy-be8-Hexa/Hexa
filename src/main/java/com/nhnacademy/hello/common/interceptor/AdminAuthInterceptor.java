313f1375392e3237e34406233537b303edf94831
iooah <iooah@naver.com>
2024-12-27 11:04:43 +0900

    fix: 오류 수정 중

--- src/main/java/com/nhnacademy/hello/common/interceptor/AdminAuthInterceptor.java
+++ src/main/java/com/nhnacademy/hello/common/interceptor/AdminAuthInterceptor.java
@@ -1,22 +1,22 @@
-package com.nhnacademy.hello.common.interceptor;
-
-import com.nhnacademy.hello.common.util.AuthInfoUtils;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.servlet.HandlerInterceptor;
-
-public class AdminAuthInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (!AuthInfoUtils.isLogin()) {
-            response.sendRedirect("/login");
-            return false;
-        }
-        if (!"ADMIN".equals(AuthInfoUtils.getRole())) {
-            response.sendRedirect("/index");
-            return false;
-        }
-        return true;
-    }
-}
-
+//package com.nhnacademy.hello.common.interceptor;
+//
+//import com.nhnacademy.hello.common.util.AuthInfoUtils;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//
+//public class AdminAuthInterceptor implements HandlerInterceptor {
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        if (!AuthInfoUtils.isLogin()) {
+//            response.sendRedirect("/login");
+//            return false;
+//        }
+//        if (!"ADMIN".equals(AuthInfoUtils.getRole())) {
+//            response.sendRedirect("/index");
+//            return false;
+//        }
+//        return true;
+//    }
+//}
+//
