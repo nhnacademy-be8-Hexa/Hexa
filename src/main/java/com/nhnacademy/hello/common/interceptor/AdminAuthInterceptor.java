e410dcd21cd742137adf91ca73066472dac70e8e
iooah <iooah@naver.com>
2024-12-27 10:57:47 +0900

    fix: 오류 수정 중

--- src/main/java/com/nhnacademy/hello/common/interceptor/AdminAuthInterceptor.java
+++ src/main/java/com/nhnacademy/hello/common/interceptor/AdminAuthInterceptor.java
@@ -6,22 +6,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 public class AdminAuthInterceptor implements HandlerInterceptor {
-
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 로그인 검사
-        if(!AuthInfoUtils.isLogin()){
+        if (!AuthInfoUtils.isLogin()) {
             response.sendRedirect("/login");
             return false;
         }
-
-        // 관리자인지 권한 검사
-        if ("ADMIN".equals(AuthInfoUtils.getRole())){
-            return true;
-        } else {
+        if (!"ADMIN".equals(AuthInfoUtils.getRole())) {
             response.sendRedirect("/index");
             return false;
         }
-
+        return true;
     }
 }
+
