a4413731bef8df36c69472e7c65450d563d4815a
조승주 <tmdwn3318@gmail.com>
2024-12-30 16:43:28 +0900

    feat: WrappingPaperAdapter

---
+++ src/main/java/com/nhnacademy/hello/common/feignclient/WrappingPaperAdapter.java
@@ -0,0 +1,37 @@
+package com.nhnacademy.hello.common.feignclient;
+
+import com.nhnacademy.hello.dto.order.WrappingPaperDTO;
+import com.nhnacademy.hello.dto.order.WrappingPaperRequestDTO;
+import jakarta.validation.Valid;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "hexa-gatewway", contextId = "wrappingPaperAdapter")
+public interface WrappingPaperAdapter {
+
+    @GetMapping
+    public List<WrappingPaperDTO> getAllWrappingPapers();
+
+    @PostMapping
+    public ResponseEntity<WrappingPaperDTO> createWrappingPaper(
+            @Valid @RequestBody WrappingPaperRequestDTO wrappingPaperRequestDTO);
+
+
+    @GetMapping("/{wrappingPaperId}")
+    public WrappingPaperDTO getWrappingPaper(
+            @PathVariable Long wrappingPaperId);
+
+    @PutMapping("/{wrappingPaperId}")
+    public ResponseEntity<WrappingPaperDTO> updateWrappingPaper(
+            @PathVariable Long wrappingPaperId,
+            @Valid @RequestBody
+            WrappingPaperRequestDTO wrappingPaperRequestDTO);
+
+    @DeleteMapping("/{wrappingPaperId}")
+    public ResponseEntity<WrappingPaperDTO> deleteWrappingPaper(
+            @PathVariable Long wrappingPaperId);
+
+}
