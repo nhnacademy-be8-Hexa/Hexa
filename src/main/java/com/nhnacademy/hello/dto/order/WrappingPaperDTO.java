79ae1e9379ab43e65cdcb1063ea9443fa311ad24
조승주 <tmdwn3318@gmail.com>
2024-12-30 16:49:51 +0900

    fix: 받아오는 dto는 검증 어노테이션 제거

--- src/main/java/com/nhnacademy/hello/dto/order/WrappingPaperDTO.java
+++ src/main/java/com/nhnacademy/hello/dto/order/WrappingPaperDTO.java
@@ -1,15 +1,8 @@
 package com.nhnacademy.hello.dto.order;
 
-import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.Length;
-
 public record WrappingPaperDTO(
         Long wrappingPaperId,
-
-        @NotBlank
-        @Length(max = 20)
         String wrappingPaperName,
-
         Integer wrappingPaperPrice
 ) {
 }
