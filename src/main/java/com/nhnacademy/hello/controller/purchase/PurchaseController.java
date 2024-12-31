b07d719e01d36c1aea47ac2fe1fd5c2c70447894
whskgys <whskgys@naver.com>
2024-12-31 18:04:15 +0900

    주문 페이지 배송 sector 추가

--- src/main/java/com/nhnacademy/hello/controller/purchase/PurchaseController.java
+++ src/main/java/com/nhnacademy/hello/controller/purchase/PurchaseController.java
@@ -1,7 +1,10 @@
 package com.nhnacademy.hello.controller.purchase;
 
 import com.nhnacademy.hello.common.feignclient.BookAdapter;
+import com.nhnacademy.hello.common.feignclient.MemberAdapter;
+import com.nhnacademy.hello.common.util.AuthInfoUtils;
 import com.nhnacademy.hello.dto.book.BookDTO;
+import com.nhnacademy.hello.dto.member.MemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,6 +33,9 @@ public class PurchaseController {
         // 전달받은 북 ids 로 책 리스트 받아와서 model 로 전달
         List<BookDTO> bookList = bookAdapter.getBooksByIds(bookIds);
         model.addAttribute("bookList", bookList);
+
+        boolean isLoggedIn = AuthInfoUtils.isLogin();
+        model.addAttribute("isLoggedIn", isLoggedIn);
 
         // 바로구매로 넘어올 시 quantity가 있음
         if(quantity != null && quantity > 0) {
