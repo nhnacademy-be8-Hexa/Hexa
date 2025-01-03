90aee5561197677095999fd54035c27e8c6c01c4
조승주 <tmdwn3318@gmail.com>
2025-01-03 16:37:35 +0900

    feat: 결제 완료 시 db에 주문 정보와 도서 저장
    포장지 선택은 아직 없으므로 Null 고정.

---
+++ src/main/java/com/nhnacademy/hello/dto/purchase/PurchaseDTO.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.hello.dto.purchase;
+
+import java.util.List;
+
+public record PurchaseDTO(
+        String paymentKey,
+        String orderId,
+        int amount,
+        List<PurchaseBookDTO> books,
+        String zoneCode,
+        String address,
+        String addressDetail
+) {
+}
+
