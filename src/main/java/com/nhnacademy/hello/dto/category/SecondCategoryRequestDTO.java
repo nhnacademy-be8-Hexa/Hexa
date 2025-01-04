2c1fc5cb4ccb3d915175c913da967a6223284def
Que-Sera-Sera-Today <30decade@gmail.com>
2025-01-04 22:09:44 +0900

    Revert "Revert "Develop""

--- src/main/java/com/nhnacademy/hello/dto/category/SecondCategoryRequestDTO.java
+++ src/main/java/com/nhnacademy/hello/dto/category/SecondCategoryRequestDTO.java
@@ -3,7 +3,6 @@ package com.nhnacademy.hello.dto.category;
 import jakarta.validation.constraints.NotNull;
 
 public record SecondCategoryRequestDTO(
-        @NotNull
         Long categoryId,
         @NotNull
         Long subCategoryId
