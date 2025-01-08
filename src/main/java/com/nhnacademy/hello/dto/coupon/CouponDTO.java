50b61600d88374e7006f11c579a9ec525359dc85
조승주 <tmdwn3318@gmail.com>
2025-01-08 18:32:50 +0900

    fix: log 제거

--- src/main/java/com/nhnacademy/hello/dto/coupon/CouponDTO.java
+++ src/main/java/com/nhnacademy/hello/dto/coupon/CouponDTO.java
@@ -1,9 +1,5 @@
 package com.nhnacademy.hello.dto.coupon;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
-
 import java.time.ZonedDateTime;
 
 public record CouponDTO(Long couponId,
