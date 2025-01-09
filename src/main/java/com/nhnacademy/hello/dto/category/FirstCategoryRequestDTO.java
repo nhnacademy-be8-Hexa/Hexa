6221c69d70dcdc9224fc0d7b946c4cf09a7e4398
kyubin <kyubinlee22@gmail.com>
2025-01-09 20:18:43 +0900

    feat: 카테고리 이름 필드 유효성 검증 로직 수정

--- src/main/java/com/nhnacademy/hello/dto/category/FirstCategoryRequestDTO.java
+++ src/main/java/com/nhnacademy/hello/dto/category/FirstCategoryRequestDTO.java
@@ -1,11 +1,12 @@
 package com.nhnacademy.hello.dto.category;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record FirstCategoryRequestDTO(
-        @Max(20)
+
         @NotBlank
+        @Size(max = 20, message = "카테고리 이름은 20자 이하로 입력해야 합니다.")
         String categoryName
 ) {
 }
