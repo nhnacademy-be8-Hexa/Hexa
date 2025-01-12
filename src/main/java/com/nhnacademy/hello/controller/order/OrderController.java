448141f37339ee0ce74a6ef8770bec3025009cde
조승주 <tmdwn3318@gmail.com>
2025-01-12 17:55:27 +0900

    feat: 관리자 처리 용 반품, 환불 처리 post 추가

--- src/main/java/com/nhnacademy/hello/controller/order/OrderController.java
+++ src/main/java/com/nhnacademy/hello/controller/order/OrderController.java
@@ -106,4 +106,49 @@ public class OrderController {
     ){
     }
 
+    // 관리자의 반품 처리
+    @PostMapping("/orders/{orderId}/return")
+    public ResponseEntity<?> returnOrder(
+            @PathVariable("orderId") Long orderId
+    ){
+
+        // 토스 결제 정보 조회
+        TossPaymentDto payment = tossPaymentAdapter.getPayment(orderId).getBody();
+
+        // 환불 비용이 3000원 이상일 경우에만 환불 해줌
+        if(payment.amount() > 3000) {
+
+            // 반품 사유 조회
+            ReturnsDTO returnsDTO = returnsAdapter.getReturnsByOrderId(orderId);
+            ReturnsReasonDTO returnsReasonDTO = returnsReasonAdapter.getReturnsReasonById(returnsDTO.returnsReasonId());
+
+            // 토스 주문 취소 처리
+            ResponseEntity<?> response = tossService.cancelPayment(
+                    payment.paymentKey(),
+                    "구매자 반품 신청: " + returnsReasonDTO.returnsReason(),
+                    payment.amount() - 3000 // 배송비 제외한 금액 환불
+            );
+
+            if(response.getStatusCode() != HttpStatus.OK) {
+                // 토스 처리 오류
+                return response;
+            }
+
+        }
+
+        // 주문 상태 변경 -> RETURNED 4
+        OrderRequestDTO orderRequestDTO = new OrderRequestDTO(
+                null,
+                null,
+                null,
+                4L,
+                null,
+                null,
+                null
+        );
+        orderAdapter.updateOrder(orderId, orderRequestDTO);
+
+        return ResponseEntity.ok().build();
+    }
+
 }
