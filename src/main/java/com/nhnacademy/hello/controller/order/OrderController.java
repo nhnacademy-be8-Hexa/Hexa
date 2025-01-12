8dcda74d032b50235ec99768500943665aa4cfcd
조승주 <tmdwn3318@gmail.com>
2025-01-12 17:54:48 +0900

    fix: 회원/비회원 주문 페이지에서, 반품 할 때 바로 환불되지 않고, 반품 신청 상태가 되도록 변경

--- src/main/java/com/nhnacademy/hello/controller/order/OrderController.java
+++ src/main/java/com/nhnacademy/hello/controller/order/OrderController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.hello.common.feignclient.ReturnsAdapter;
 import com.nhnacademy.hello.common.feignclient.ReturnsReasonAdapter;
 import com.nhnacademy.hello.common.feignclient.payment.TossPaymentAdapter;
 import com.nhnacademy.hello.dto.order.OrderRequestDTO;
+import com.nhnacademy.hello.dto.returns.ReturnsDTO;
 import com.nhnacademy.hello.dto.returns.ReturnsReasonDTO;
 import com.nhnacademy.hello.dto.returns.ReturnsRequestDTO;
 import com.nhnacademy.hello.dto.toss.TossPaymentDto;
@@ -66,43 +67,22 @@ public class OrderController {
         return response;
     }
 
-    @PostMapping("/orders/{orderId}/return")
-    public ResponseEntity<?> returnOrder(
+    // 사용자의 반품 신청
+    @PostMapping("/orders/{orderId}/return-request")
+    public ResponseEntity<?> returnRequestOrder(
             @PathVariable("orderId") Long orderId,
             @RequestBody ReturnsRequest returnsRequest
             ){
 
-
         // 반품 사유 조회
         ReturnsReasonDTO returnsReasonDTO = returnsReasonAdapter.getReturnsReasonById(returnsRequest.reasonId());
 
-
-        // 결제 정보 조회
-        TossPaymentDto payment = tossPaymentAdapter.getPayment(orderId).getBody();
-
-        // 환불 비용이 3000원 이상일 경우에만 환불 해줌
-        if(payment.amount() > 3000) {
-
-            // 토스 주문 취소 처리
-            ResponseEntity<?> response = tossService.cancelPayment(
-                    payment.paymentKey(),
-                    "구매자 반품 처리: " + returnsReasonDTO.returnsReason(),
-                    payment.amount() - 3000 // 배송비 제외한 금액 환불
-            );
-
-            if(response.getStatusCode() != HttpStatus.OK) {
-                // 토스 처리 오류
-                return response;
-            }
-
-        }
-
-        // 주문 상태 변경 -> RETURNED 4
+        // 주문 상태 변경 -> RETURN_REQUEST 6
         OrderRequestDTO orderRequestDTO = new OrderRequestDTO(
                 null,
                 null,
                 null,
-                4L,
+                6L,
                 null,
                 null,
                 null
