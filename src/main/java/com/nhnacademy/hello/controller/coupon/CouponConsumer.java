33afad97197b51605b024e5e19904ec420cb64b5
highsiverr <ye90202@gmail.com>
2025-01-15 15:04:41 +0900

    perf: 쿠폰 발급 로직 어탭터 최소화

--- src/main/java/com/nhnacademy/hello/controller/coupon/CouponConsumer.java
+++ src/main/java/com/nhnacademy/hello/controller/coupon/CouponConsumer.java
@@ -9,9 +9,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.core.Message;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Component
 @AllArgsConstructor
@@ -28,24 +26,32 @@ public class CouponConsumer {
         String memberId = new String(Objects.requireNonNull(message).getBody());
 
         List<CouponMemberDTO> couponMember = couponMemberAdapter.getMemberCoupons(memberId); // 멤버가 가진 쿠폰들
-        List<String> couponNameMember = new ArrayList<>(); // 멤버가 가진 쿠폰 이름을 저장할 변수 선언
+        List<Long> couponIds = new ArrayList<>(); // 멤버가 가진 쿠폰 아이디를 저장 할 리스트 선언
 
-        for (CouponMemberDTO memberDTO : couponMember) { // 멤버가 가진 쿠폰 아이디 하나하나 돌면서 쿠폰 이름을 찾는 반복문
-            CouponDTO couponIdMember = couponAdapter.getCouponById(memberDTO.couponId()); // 멤버가 가진 쿠폰 아이디로 쿠폰이름 찾기
-            couponNameMember.add(couponIdMember.couponName()); // 멤버가 가진 쿠폰 이름 리스트에 넣기
+        for (CouponMemberDTO couponMemberDTO : couponMember) { // 멤버들이 가진 쿠폰들을 순회
+            couponIds.add(couponMemberDTO.couponId()); // 쿠폰 객체에서 쿠폰 아이디만 불러와 저장
+        }
+
+        List<CouponDTO> couponTrue = couponAdapter.getCouponsByActive(couponIds, true); // 활성화된 쿠폰 조회
+        List<CouponDTO> couponFalse = couponAdapter.getCouponsByActive(couponIds, false); // 비활성화된 쿠폰 조회
+
+        Set<String> couponNameMember = new HashSet<>(); // 멤버가 가진 쿠폰 이름을 담을 리스트 // 어레이리시트를 해시셋으로 바꿈
+
+        // 활성화된 쿠폰 + 비활성화된 쿠폰
+        for (CouponDTO dto : couponTrue) {
+            couponNameMember.add(dto.couponName());
+        }
+        for(CouponDTO dto : couponFalse){
+            couponNameMember.add(dto.couponName());
         }
 
         List<CouponDTO> coupon = couponAdapter.getCouponByCouponName("겨울 쿠폰"); // 만들어진 쿠폰 중 멤버에게 나눠줄 쿠폰을 쿠폰 이름으로 찾기
 
-        //boolean isCouponName = false;
         for (CouponDTO couponDTO : coupon) {
-            for (String s : couponNameMember) {
-                if (couponDTO.couponName().equals(s) && !couponMemberAdapter.checkCouponDuplicate(couponDTO.couponId())) { // 멤버가 가진 쿠폰 중 받으려는 쿠폰과 이름이 같은 쿠폰이 있는지 조사
-                    return "이미 발급받은 쿠폰입니다.";
-//                    isCouponName = true; // 있다면 참
-                }
+            if (couponNameMember.contains(couponDTO.couponName())) { // 멤버가 받은 적 없는 쿠폰이어야 함
+                return "이미 발급받은 쿠폰입니다.";
             }
-            if(!couponMemberAdapter.checkCouponDuplicate(couponDTO.couponId())){ // 다른 사람이 받은 쿠폰이 아니고 멤버가 받은 적 없는 쿠폰이어야 함
+            if(!couponMemberAdapter.checkCouponDuplicate(couponDTO.couponId())){ // 다른 사람이 받은 쿠폰이 아니여야 함 // 너무 많은 페인 클라이언트 호출 -> 멤버쿠폰 테이블에 있는 아이디를 모두 뽑아와서 하는게 비교하는게 효율적일듯
                 couponMemberAdapter.createMemberCoupon(memberId, couponDTO.couponId());
                 return "쿠폰을 발급 받았습니다!";
             }
