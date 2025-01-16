39dee3f64daf55df04340b9322008a1bdc566a2c
kyubin <kyubinlee22@gmail.com>
2025-01-16 14:03:29 +0900

    feat : 외부 도서 등록 기능 추가

--- src/main/java/com/nhnacademy/hello/controller/admin/AladinApiController.java
+++ src/main/java/com/nhnacademy/hello/controller/admin/AladinApiController.java
@@ -1,12 +1,20 @@
 package com.nhnacademy.hello.controller.admin;
 
 import com.nhnacademy.hello.common.feignclient.AladinApiAdapter;
+import com.nhnacademy.hello.common.feignclient.AuthorAdapter;
+import com.nhnacademy.hello.common.feignclient.BookAdapter;
+import com.nhnacademy.hello.common.feignclient.PublisherAdapter;
 import com.nhnacademy.hello.dto.book.AladinBookDTO;
+import com.nhnacademy.hello.dto.book.AladinBookRequestDTO;
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +24,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AladinApiController {
 
     private final AladinApiAdapter aladinApiAdapter;
+    private final BookAdapter bookAdapter;
+    private final AuthorAdapter authorAdapter;
+    private final PublisherAdapter publisherAdapter;
 
     @GetMapping
     public String aladinApi(@RequestParam(required = false) String query, Model model) {
@@ -23,5 +34,11 @@ public class AladinApiController {
         model.addAttribute("books", books);
         model.addAttribute("query", query);
         return "admin/aladinBookSearch";
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createAladinBook(@Valid @RequestBody AladinBookRequestDTO aladinBookRequestDTO) {
+        aladinApiAdapter.createAladinBook(aladinBookRequestDTO);
+        return ResponseEntity.ok().build();
     }
 }
